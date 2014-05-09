fb5af3ad203624586aa9e3b6cf4fbb043954d109
Jasper Haggenburg <jasperh@q42.nl>
2014-05-09 08:05:27 +0200

    basic setup

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: ['src/base.scss', 'src/mixins.scss'],
+        dest: 'dist/T-1000.scss'
+      }
+    },
+
+    watch: {
+      concat: {
+        files: '<%= concat.dist.src %>',
+        tasks: ['concat:dist']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['concat']);
+};
